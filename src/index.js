53aa94ba0f7692b013ad6b3e14f36cb8768bf9ef
Michael Bowlin <jaidoutani@gmail.com>
2022-03-18 00:51:48 -0500

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,30 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+    let form = document.getElementById('create-task-form')
+    form.addEventListener('submit', (e) => {
+        e.preventDefault()
+        let value = document.getElementById('new-task-description').value
+        addTasks(value)
+    })
+    let taskUl = document.getElementById('tasks')
+    taskUl.addEventListener('click', (e) => {
+        e.preventDefault()
+        if(e.target.innerHTML === "x"){
+            handleDelete(e)
+        }
+        console.log(e)
+    })
 });
+
+function addTasks(task) {
+    let liTag = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.innerHTML = 'x'
+    liTag.textContent = `${task} `
+    liTag.appendChild(btn)
+    let ulTasks = document.getElementById('tasks')
+    ulTasks.appendChild(liTag)
+}
+
+function handleDelete(e) {
+    e.target.parentNode.remove()
+}
